1651508518822504ecb2ba4686473a8a7ad5212e
PedroAmaral05 <pedro.pinto.amaral@escola.pr.gov.br>
2026-08-20 09:12:39 -0300

    sdfes

--- main.js
+++ main.js
@@ -81,7 +81,7 @@ let perguntaAtual;
 let historiaFinal = ""
 
 function mostraPergunta(){
-    if (atual >= perguntas.length){
+    if(atual >= perguntas.length){
         mostraResultado()
         return
     }
