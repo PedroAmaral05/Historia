2ec1daf049e17b28a4244a957f42ef2ca7a48721
PedroAmaral05 <pedro.pinto.amaral@escola.pr.gov.br>
2026-08-25 11:53:05 -0300

    huieffi

--- main.js
+++ main.js
@@ -86,12 +86,15 @@ function mostraPergunta(){
     }
     perguntaAtual = perguntas [atual]
     caixaPerguntas.textContent =perguntaAtual.enunciado
+    caixaAlternativas.textContent = "";
     mostraAlternativas()
 }
 
 function mostraAlternativas (){
     for (const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button")
+        botaoAlternativas.textContent =alternativa.texto
+        botaoAlternativas.addEventListener("click", ()=> respostaSelecionada(alternativa))
     }
 }
 mostraPergunta()
