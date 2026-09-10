fc5fccae984f5e3089bea69f99c47641b28573ac
PedroAmaral05 <pedro.pinto.amaral@escola.pr.gov.br>
2026-09-10 09:23:08 -0300

    ko

--- main.js
+++ main.js
@@ -10,11 +10,14 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Lançar um saque viagem neles!!",
-                afirmacao: "Kazuki estava voltando da escola para casa, quando um grupo de adolescentes rebeldes o abordaram e anunciaram um assalto, exigindo seu troféu de MVP da Liga. Para se defender Artur mira um saque viagem entre os ladrões, ricocheteando a bola entre eles até um monte de areia próximo, levantando a areia e cegando seus oponentes."
+                afirmacao:[ "Kazuki estava voltando da escola para casa, quando um grupo de adolescentes rebeldes o abordaram e anunciaram um assalto, exigindo seu troféu de MVP da Liga. Para se defender Artur mira um saque viagem entre os ladrões, ricocheteando a bola entre eles até um monte de areia próximo, levantando a areia e cegando seus oponentes.",
+                    "texto2"
+                ]
             },
             {
                 texto: "Entregar o seu troféu :(",
-                afirmacao: "Kazuki estava voltando da escola para casa, quando um grupo de adolescentes rebeldes o abordaram e anunciaram um assalto, exigindo seu troféu de MVP da Liga. Acuado, Kazuki entrega o seu troféu, os rebeldes DO MAL o pegam e exigem mais pertences."
+                afirmacao:[ "Kazuki estava voltando da escola para casa, quando um grupo de adolescentes rebeldes o abordaram e anunciaram um assalto, exigindo seu troféu de MVP da Liga. Acuado, Kazuki entrega o seu troféu, os rebeldes DO MAL o pegam e exigem mais pertences.", 
+                    "texto2"]
             }
 
 
@@ -26,11 +29,13 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Quebrar o aspirador",
-                afirmacao: "Os ladrões afirmam que não querem apenas o troféu, mas todas as suas habilidades de vôlei, utilizando um aspirador para tentar roubar a energia de volei de Kazuki, que acerta uma machete no aspirador e o isola!!",
+                afirmacao:[ "Os ladrões afirmam que não querem apenas o troféu, mas todas as suas habilidades de vôlei, utilizando um aspirador para tentar roubar a energia de volei de Kazuki, que acerta uma machete no aspirador e o isola!!", 
+                    "texto2"]
             },
             {
                 texto: "Desviar",
-                afirmacao: "Os ladrões afirmam que não querem apenas o troféu, mas todas as suas habilidades de vôlei, utilizando um aspirador para tentar roubar a energia de volei de Kazuki. Tentando desviar, ele perde 5% de suas habilidades de vôlei, ficando mais fraco.",
+                afirmacao:[ "Os ladrões afirmam que não querem apenas o troféu, mas todas as suas habilidades de vôlei, utilizando um aspirador para tentar roubar a energia de volei de Kazuki. Tentando desviar, ele perde 5% de suas habilidades de vôlei, ficando mais fraco.", 
+                    "texto2"]
             }
         ]
     },
@@ -39,24 +44,28 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Jogar um Saque viagem para combater o saque flutuante unido poderoso deles",
-                afirmacao: "Observando a ineficácia do aspirador, os ladrões decidem jogar o jogo de Kazuki. Os 3 lançam um saque flutuante, unidos para derrotar Kazuki. Artur se defende jogando um saque viajante para combater o saque flutuante unido poderoso deles. A bola de Kazuki gira contra a deles, durante um tempo, porém, o saque de Kazuki se mostra mais poderoso e atravessa a bola dos inimigos, voando em direção deles.",
+                afirmacao:[ "Observando a ineficácia do aspirador, os ladrões decidem jogar o jogo de Kazuki. Os 3 lançam um saque flutuante, unidos para derrotar Kazuki. Artur se defende jogando um saque viajante para combater o saque flutuante unido poderoso deles. A bola de Kazuki gira contra a deles, durante um tempo, porém, o saque de Kazuki se mostra mais poderoso e atravessa a bola dos inimigos, voando em direção deles.", 
+                    "texto2"]
             },
             {
                 texto: "Bloquear o Saque com as mãos",
-                afirmacao: "Observando a ineficácia do aspirador, os ladrões decidem jogar o jogo de Kazuki. Os 3 lançam um saque flutuante, unidos para derrotar Kazuki. Artur se defende bloqueando o saque com as mãos. Após muito esforço, Kazuki redireciona a bola para eles, somado com a sua força, a bola atinge todos eles.",
+                afirmacao:[ "Observando a ineficácia do aspirador, os ladrões decidem jogar o jogo de Kazuki. Os 3 lançam um saque flutuante, unidos para derrotar Kazuki. Artur se defende bloqueando o saque com as mãos. Após muito esforço, Kazuki redireciona a bola para eles, somado com a sua força, a bola atinge todos eles.", 
+                    "texto2"]
             }
         ]
     },
     {
-        enunciado: "A poeira levanta... os inimigos estão todos no chão, sem conseguir se mover, Kazuki se agarra ao seu troféu quase perdido. Os inimigos pedem misericórdia, e pedem para Kazuki ser o mestre deles.",
+        enunciado: "A poeira levanta... os inimigos estão todos no chão, sem conseguir se mover, Kazuki se agarra ao seu troféu quase perdido. Os inimigos pedem misericórdia, e pedem para Kazuki ser o mestre deles."
         alternativas: [
             {
                 texto: "Aceitar",
-                afirmacao: "Os inimigos estão todos no chão...pedindo misericórdia e que Kazuki se torne o mestre de vôlei deles. Kazuki aceita o pedido. Após 20 anos treinando seus pupilos, Kazuki não possuia muito tempo para si, e não obtém sucesso no mundial, porém, todos seus pupilos(que não maltratam mais animais de rua), obtiveram o primeiro lugar no mundial, os 3 sendo considerados os melhores de suas equipes independente da posição. Eles indicam Kazuki mundialmente, que funda a 'Kazukinhos Do Volêi', a escola mais renomada escola de volêi, procurada mundialmente. Kazuki se aposenta com 32 anos, milionário, considerado o melhor professor de volêi do mundo. Kazuki considerado um sensei do volêi, atinge um estado de paz de espírito tão grande, que atinge o nirvana, vivendo por longos 250 anos.",
+                afirmacao:[ "Os inimigos estão todos no chão...pedindo misericórdia e que Kazuki se torne o mestre de vôlei deles. Kazuki aceita o pedido. Após 20 anos treinando seus pupilos, Kazuki não possuia muito tempo para si, e não obtém sucesso no mundial, porém, todos seus pupilos(que não maltratam mais animais de rua), obtiveram o primeiro lugar no mundial, os 3 sendo considerados os melhores de suas equipes independente da posição. Eles indicam Kazuki mundialmente, que funda a 'Kazukinhos Do Volêi', a escola mais renomada escola de volêi, procurada mundialmente. Kazuki se aposenta com 32 anos, milionário, considerado o melhor professor de volêi do mundo. Kazuki considerado um sensei do volêi, atinge um estado de paz de espírito tão grande, que atinge o nirvana, vivendo por longos 250 anos.",
+                    "texto2"]
             },
             {
                 texto: "Negar",
-                afirmacao: "Os inimigos estão todos no chão...pedindo misericórdia e que Kazuki se torne o mestre de vôlei deles. Kazuki nega o pedido deles, e envia todos em direção ao sol com um saque chapado. 20 anos depois, Kazuki ainda jovem se torna o melhor atleta de volêi do mundo. Considerado pela sua técnica impecável, e jogadas quase impossíveis. Kazuki já foi colocado para jogar em um time com crianças de 2 anos contra atletas profissionais, e ainda assim, venceu. Infelizmente, aos 67 anos, Kazuki falece após se engasgar com água, pois tinha se engasgado com um amendoim, e foi beber água e se engasgou de novo. O mundo inteiro parou para enaltecer o rei do volêi, e nunca mais na história, nasceu alguém tão bom quanto o Kazuki.",
+                afirmacao:[ "Os inimigos estão todos no chão...pedindo misericórdia e que Kazuki se torne o mestre de vôlei deles. Kazuki nega o pedido deles, e envia todos em direção ao sol com um saque chapado. 20 anos depois, Kazuki ainda jovem se torna o melhor atleta de volêi do mundo. Considerado pela sua técnica impecável, e jogadas quase impossíveis. Kazuki já foi colocado para jogar em um time com crianças de 2 anos contra atletas profissionais, e ainda assim, venceu. Infelizmente, aos 67 anos, Kazuki falece após se engasgar com água, pois tinha se engasgado com um amendoim, e foi beber água e se engasgou de novo. O mundo inteiro parou para enaltecer o rei do volêi, e nunca mais na história, nasceu alguém tão bom quanto o Kazuki.", 
+                    "texto2"]
             }
         ]
     },
@@ -65,11 +74,13 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Descansar juntos dos melhores do vôlei",
-                afirmacao: "No pós-vida, Kazuki se encontra no vôleihalla, conhecendo grandes nomes do esporte como Giba, Serginho, Ana Moser e Sheilla Castro. E possui duas opções, se concretizar naquela terra sendo mais um entre os melhores jogadores de vôlei que já existiram, ou pode tentar desafiar os deuses do vôlei. Kazuki decide descansar em paz entre os melhores do vôlei, porém, eles o consideram a maior lenda a pisar naquele reino, e Kazuki recebe o título de 'Monge do vôlei', operando os campeonatos daquela terra. Além disso, ele é considerado para futuramente ser o sucessor de Volnus, o senhor das bolas e das redes.",
+                afirmacao:[ "No pós-vida, Kazuki se encontra no vôleihalla, conhecendo grandes nomes do esporte como Giba, Serginho, Ana Moser e Sheilla Castro. E possui duas opções, se concretizar naquela terra sendo mais um entre os melhores jogadores de vôlei que já existiram, ou pode tentar desafiar os deuses do vôlei. Kazuki decide descansar em paz entre os melhores do vôlei, porém, eles o consideram a maior lenda a pisar naquele reino, e Kazuki recebe o título de 'Monge do vôlei', operando os campeonatos daquela terra. Além disso, ele é considerado para futuramente ser o sucessor de Volnus, o senhor das bolas e das redes.",
+                    "texto2"]
             },
             {
                 texto: "Desafiar os Deuses do Vôlei",
-                afirmacao: "No pós-vida, Kazuki se encontra no vôleihalla, conhecendo grandes nomes do esporte como Giba, Serginho, Ana Moser e Sheilla Castro. E possui duas opções, se concretizar naquela terra sendo mais um entre os melhores jogadores de vôlei que já existiram, ou pode tentar desafiar os deuses do vôlei. Kazuki decide desafiar os deuses do vôlei, enfrentando Volnus, o Senhor das bolas e das redes, porém esse Deus era muito poderoso. Em uma partida acirrada, a divindade percebe a ameaça que Kazuki representa, e o bane após sua derrota. Atualmente, Kazuki vaga como um fantasma pela terra, procurando um dia alguém que fosse melhor que ele, torcendo para que esse indivíduo seja capaz de derrotar Volnus, e removê-lo do exílio de vôleihalla.",
+                afirmacao:[ "No pós-vida, Kazuki se encontra no vôleihalla, conhecendo grandes nomes do esporte como Giba, Serginho, Ana Moser e Sheilla Castro. E possui duas opções, se concretizar naquela terra sendo mais um entre os melhores jogadores de vôlei que já existiram, ou pode tentar desafiar os deuses do vôlei. Kazuki decide desafiar os deuses do vôlei, enfrentando Volnus, o Senhor das bolas e das redes, porém esse Deus era muito poderoso. Em uma partida acirrada, a divindade percebe a ameaça que Kazuki representa, e o bane após sua derrota. Atualmente, Kazuki vaga como um fantasma pela terra, procurando um dia alguém que fosse melhor que ele, torcendo para que esse indivíduo seja capaz de derrotar Volnus, e removê-lo do exílio de vôleihalla.",
+                    "texto2"]
             }
         ]
     },
