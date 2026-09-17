79db838d86725095e2f7b029eda442a115d6b286
PedroAmaral05 <pedro.pinto.amaral@escola.pr.gov.br>
2026-09-17 09:29:28 -0300

    oj

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-import { aleatorio } from "./aleatorio"
-import {perguntas} from "./pergunta"
+import { aleatorio } from "./aleatorio.js"
+import {perguntas} from "./pergunta.js"
 const caixaPrincipal = document.querySelector(".caixa-principal")
 const caixaPerguntas = document.querySelector(".caixa-perguntas")
 const caixaAlternativas = document.querySelector(".caixa-alternativas")
